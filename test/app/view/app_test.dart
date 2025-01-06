7321c7e7b7ed51bda041f16997da102c2f32da96
Fellipe <fellipe1311.fb@gmail.com>
2025-01-06 05:51:27 -0300

    remove counter and shadowBox

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nota_mais/app/app.dart';
-import 'package:nota_mais/counter/counter.dart';
+import 'package:nota_mais/login/view/login_view.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders LoginView', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(LoginView), findsOneWidget);
     });
   });
 }
